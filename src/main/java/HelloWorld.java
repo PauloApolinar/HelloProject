27ae6b3ae9663e21fb27fbb4b119eaa674d9b7ca
powlow10 <paulo_apolinar@hotmail.com>
2015-01-15 20:58:20 -0800

    Importing HelloProject project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aightts");
+	}
+
+}
